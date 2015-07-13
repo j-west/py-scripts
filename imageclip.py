719bc8e31b61ab19a4b996d1b88a0ff881bce8ec
j-west <jonathanrcwest@gmail.com>
2015-07-13 09:58:15 -0600

    Figured out a working regex.

--- imageclip.py
+++ imageclip.py
@@ -4,5 +4,15 @@
 # TODO: Specify size?
 
 import re
-import pyperclip
+#import pyperclip
 
+imgREGEX = re.compile(r'''(
+	https			#start of line?
+	://
+	[a-zA-Z0-9.-]+		#anything before .jpg
+	\.jpg|png		#file type
+	)''', re.VERBOSE)
+
+mo = imgREGEX.findall('hereisaurlhttps://blabla.co.jpglololol')
+
+print(mo)
