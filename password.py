92a56c832ee45b785099c2e8b50c3022ef71340f
j-west <jonathanrcwest@gmail.com>
2015-07-26 15:29:37 -0600

    This is disgusting but it works.  Separated Regex statements.

--- password.py
+++ password.py
@@ -6,17 +6,30 @@
 #						-At least 1 digit
 
 import re
-charREGEX = re.compile(r'''(
-		\S{8,}
-		&[a-z]+
-		&[A-Z]+
-		&[0-9]+
-		&\W+
-		)''', re.VERBOSE)
+#charREGEX = re.compile(r'''(
+#		\S{8,}
+#		&[a-z]+
+#		&[A-Z]+
+#		&[0-9]+
+#		&\W+
+#		)''', re.VERBOSE)
+
+#password = input()
+
+#if charREGEX.search(password):
+#	print('I will accept this.')
+#else:
+#	print ('No. Try again.')
+
+#---Separate Logic Statements?---
+
+charREGEX = re.compile(r'\S{8,}')
+lowerREGEX = re.compile(r'[a-z]+')
+upperREGEX = re.compile(r'[A-Z]+')
+numREGEX = re.compile(r'[0-9]+')
 
 password = input()
-
-if charREGEX.search(password):
-	print('I will accept this.')
+if re.search(charREGEX, password) and re.search(lowerREGEX, password) and re.search(upperREGEX, password) and re.search(numREGEX, password):
+	print('good enough.')
 else:
-	print ('No. Try again.')
+	print('Doesn\'t qualify.')
