ef4184dc3f752967a45d1cf9786948f94bc5b6f7
j-west <jonathanrcwest@gmail.com>
2015-07-26 20:06:43 -0600

    Cleaned up code (proper commit this time).

--- password.py
+++ password.py
@@ -1,28 +1,15 @@
 #! 	python3
+
+#TODO: Find a way to combine these regex's.
+
 #  	password.py - Testing Password Strength w/ REGEX
 
 #	STRONG PASSWORD	 =	-8 or more characters
 #						-Both UPPER and lower case
 #						-At least 1 digit
 
-import re
-
-#---Attempt at one Long REGEX---
-#charREGEX = re.compile(r'''(
-#		\S{8,}
-#		&[a-z]+
-#		&[A-Z]+
-#		&[0-9]+
-#		&\W+
-#		)''', re.VERBOSE)
-
-#password = input()
-
-#if charREGEX.search(password):
-#	print('I will accept this.')
-#else:
-#	print ('No. Try again.')
-
+import re, getpass
+#getpass: prompts user for password, imported for concealment.
 
 #---Separate Logic Statements?---
 charREGEX = re.compile(r'\S{8,}')
@@ -30,8 +17,8 @@ lowerREGEX = re.compile(r'[a-z]+')
 upperREGEX = re.compile(r'[A-Z]+')
 numREGEX = re.compile(r'[0-9]+')
 
-password = input()
+password = getpass.getpass()
 if re.search(charREGEX, password) and re.search(lowerREGEX, password) and re.search(upperREGEX, password) and re.search(numREGEX, password):
-	print('good enough.')
+	print('Good Enough.')
 else:
-	print('Doesn\'t qualify.')
+	print('Try again.  Your password needs one upper and one lower case letter, a number, and needs to be at least 8 characters.')
